09e7c603b717718c270d75e47dbead745395a4ae
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-02-15 19:04:43 +0100

    Completed lists-cond-01

--- ConditionalContent/lists-cond-01-starting-setup/app.js
+++ ConditionalContent/lists-cond-01-starting-setup/app.js
@@ -1,7 +1,18 @@
 const app = Vue.createApp({
-  data() {
-    return { goals: [] };
-  },
+    data() {
+        return {
+            enteredValue: '',
+            goals: [],
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+        },
+        removeGoal(index) {
+            this.goals.splice(index, 1);
+        }
+    }
 });
 
 app.mount('#user-goals');
