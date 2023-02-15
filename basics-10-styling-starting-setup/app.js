44cc9e0120aad10e4da843d38b14044936c61db2
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-02-15 16:06:50 +0100

    Completed basics-10-styling-starting-setup

---
+++ basics-10-styling-starting-setup/app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false,
+        };
+    },
+    computed: {
+        boxAClasses() {
+            return { active: this.boxASelected };
+        }
+    },
+    methods: {
+        boxSelected(box) {
+            switch (box) {
+                case 'A':
+                    this.boxASelected = !this.boxASelected;
+                    break;
+                case 'B':
+                    this.boxBSelected = !this.boxBSelected;
+                    break;
+                case 'C':
+                    this.boxCSelected = !this.boxCSelected;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+});
+
+app.mount('#styling');
