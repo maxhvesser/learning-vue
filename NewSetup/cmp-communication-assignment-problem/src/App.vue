7194c5c0ef04dcdd60b3db97d0f06e7845dad821
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-02-19 20:57:09 +0100

    Completed cmp-communication-assignment-problem

---
+++ NewSetup/cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <active-user
+      :age="activeUser.age"
+      :username="activeUser.username"
+  ></active-user>
+  <user-data
+    @user-data="updateEnteredUser"
+  ></user-data>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      activeUser: {
+        username: '',
+        age: '',
+      },
+    };
+  },
+  methods: {
+    updateEnteredUser(username, age) {
+      this.activeUser = {
+        username: username,
+        age: age,
+      };
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid lightgray;
+  padding: 1rem;
+}
+</style>
---
+++ NewSetup/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <form @submit.prevent="emitActiveUser">
+      <label>Username</label>
+      <input type="text" v-model="enteredUsername"/>
+      <label>Age</label>
+      <input type="text" v-model="enteredAge"/>
+      <button>Set Active User</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "UserData",
+  data() {
+    return {
+      enteredUsername: '',
+      enteredAge: '',
+    };
+  },
+  emits: ['user-data'],
+  methods: {
+    emitActiveUser() {
+      this.$emit(
+          'user-data',
+          this.enteredUsername,
+          this.enteredAge,
+      );
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
